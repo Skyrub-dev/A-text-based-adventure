9f77c6c82862095a2c699d490555ed788dfad57d
Skyrub-dev <75093597+Skyrub-dev@users.noreply.github.com>
2021-08-14 16:48:27 +0100

    Made a menu to start

--- text-based-adventure/Program.cs
+++ text-based-adventure/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace text_based_adventure
 {
     class Program
     {
+        static void warning()
+        {
+            Console.SetCursorPosition(1, 50);
+            Console.WriteLine("Loading...  |  Please note: This project is a WIP");
+        }
+        
+        static void menu()
+        {
+            Console.SetCursorPosition(50, 0);
+            Console.WriteLine("Welcome to...");
+            string intro = @"
+ ______      __                        __                       __     
+/\  _  \    /\ \                      /\ \__                   /\ \    
+\ \ \L\ \   \_\ \  __  __    __    ___\ \ ,_\ __  __  _ __   __\ \ \   
+ \ \  __ \  /'_` \/\ \/\ \ /'__`\/' _ `\ \ \//\ \/\ \/\`'__/'__`\ \ \  
+  \ \ \/\ \/\ \L\ \ \ \_/ /\  __//\ \/\ \ \ \\ \ \_\ \ \ \/\  __/\ \_\ 
+   \ \_\ \_\ \___,_\ \___/\ \____\ \_\ \_\ \__\ \____/\ \_\ \____\\/\_\
+    \/_/\/_/\/__,_ /\/__/  \/____/\/_/\/_/\/__/\/___/  \/_/\/____/ \/_/
+                                                                       
+                                                                                                 
+                            ";
+            Console.WriteLine(intro);
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("insert welcome ascii here");
+            warning();
+            var pause = Task.Delay(2000);
+            pause.Wait();
+            Console.Clear();
+            menu();
+            //Console.WriteLine("insert welcome ascii here");
         }
     }
 }
