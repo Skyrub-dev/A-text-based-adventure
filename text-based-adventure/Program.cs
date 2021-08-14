ac0d2c97662dbbe41dc9541fe258f0a37d0021da
Skyrub-dev <75093597+Skyrub-dev@users.noreply.github.com>
2021-08-14 12:07:11 +0100

    Add project files.

---
+++ text-based-adventure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace text_based_adventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("insert welcome ascii here");
+        }
+    }
+}
